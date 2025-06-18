75ed22958a7e715d51b3dc0cb7e4226116bd7a40
Simon Maarawi <maarawisimon8@gmail.com>
2025-06-18 19:39:28 +0300

    feat: add .env and .env.example for API config

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,28 +1,28 @@
 import axios from "axios";
 
-const BASE_URL='http://localhost:5000';
+const API_URL=import.meta.env.VITE_API_URL;;
 
 export const getNotes=async () => {
-    const response=await axios.get(`${BASE_URL}/notes`);
+    const response=await axios.get(`${API_URL}/notes`);
     return response.data;
 };
 
 export const getNote=async (title: string) => {
-    const response=await axios.get(`${BASE_URL}/notes/${encodeURIComponent(title)}`);
+    const response=await axios.get(`${API_URL}/notes/${encodeURIComponent(title)}`);
     return response.data;
 };
 
 export const addNote=async (note: {title: string, body: string, color: string}) => {
-    const response=await axios.post(`${BASE_URL}/notes`, note);
+    const response=await axios.post(`${API_URL}/notes`, note);
     return response.data;
 };
 
 export const deleteNote=async (title: string) => {
-    const response=await axios.delete(`${BASE_URL}/notes/${encodeURIComponent(title)}`);
+    const response=await axios.delete(`${API_URL}/notes/${encodeURIComponent(title)}`);
     return response.data;
 }
 
 export const updateNote=async (title: string, updatedFields: {title?: string; body?: string}) => {
-    const response=await axios.patch(`${BASE_URL}/notes/${encodeURIComponent(title)}`, updatedFields);
+    const response=await axios.patch(`${API_URL}/notes/${encodeURIComponent(title)}`, updatedFields);
     return response.data;
 }
